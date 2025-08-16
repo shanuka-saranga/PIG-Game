daba110b61840439949bf603fd46d12e5644e32f
Shanu974 <Shanukasaranga974@gmail.com>
2025-08-16 16:06:07 +0530

    sarting conditions

--- script.js
+++ script.js
@@ -2,3 +2,12 @@
 
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+const btNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btHold = document.querySelector('.btn--hold');
+
+//starting conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
